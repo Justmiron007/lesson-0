adf22c4db032efebb226eebc9b25b524a7754a23
Justmiron007 <raen.96@mail.ru>
2024-05-28 00:19:13 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+a = 'У лукоморья дуб зеленый'
+print(len (a))
+
+first = 10
+second = 5
+summa = (first + second)
+diff = (first - second)
+print(summa, second)
+
+first = 2
+second = 3
+third = 4
+mean = ((first + second + third) // 3)
+print(mean)
+
+first_string = 'Вторник'
+second_string = 'Понидельник'
+print(second_string + ', ' + first_string)
+
+a = 2
+b = 3
+c = 4
+f = (a * b) + (a * c)
+print(f ** 3 // 2)
